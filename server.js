95cf25831a7208ce92ba4ff9e90bd568a6e2cd49
romerotac <christian.romero02.cr@gmail.com>
2022-07-23 15:42:13 -0400

    first commit

--- server.js
+++ server.js
@@ -1,35 +1,68 @@
-import express from 'express';
-import React from 'react';
-import {renderToString} from 'react-dom/server';
-import { Home } from './src/pages/home';
-import {StaticRouter} from 'react-router-dom/server';
-import App from './src/App';
-import fs from 'fs';
-import path from 'path';
-import SpotifyWebApi from 'spotify-web-api-node';
 
-const app = express();
+const express = require('express');
+const SpotifyWebApi = require('spotify-web-api-node');
+const cors = require('cors');
+const bodyParser = require('body-parser');
+;
 
-app.use(express.static('./build', {index:false}))
+const port = 3001
+const app = express()
 
-app.get('/*', (req, res) => {
-    const reactApp = renderToString(
-        <StaticRouter location={req.url}>
-            <App/>
-        </StaticRouter>
-    );
+app.use(cors())
+app.use(bodyParser.json())
 
-    const templeteFile = path.resolve('./build/index.html');
-    fs.readFile(templeteFile,'utf8', (err, data) => {
-        if (err){
-            return res.status(500).send(err);
-        }
-        return res.send(
-            data.replace('<div id = "root"></div>',`<div id="root">${reactApp}</div>`)
-        )
-    });
-});
 
-app.listen(8080, () => {
-    console.log('Server is listening on port 8080');
+app.post('/refresh', (res,req) => {
+    const refreshToken = req.body.refreshToken
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri:'http://localhost:3000',
+        clientId: '49fb612d070040f9a34236837ab79ad3',
+        clientSecret: '4a45e17ddf794a1a89a32dd345095f01',
+        refreshToken      
+    })
+    spotifyApi
+      .refreshAccessToken().then((data) => {
+        res.json({
+            accessToken:data.body.accessToken,
+            expiresIn:data.body.expiresIn,
+        } )
+      }).catch(() => {
+        //res.sendStatus(400)
+      })
+      }
+    
+)
+
+
+
+app.post('/login', (req,res) => {
+    const code = req.body.code
+    const spotifyApi = new SpotifyWebApi({
+        redirectUri:'http://localhost:3000',
+        clientId: '49fb612d070040f9a34236837ab79ad3',
+        clientSecret: '4a45e17ddf794a1a89a32dd345095f01',
+    })
+
+    spotifyApi
+    .authorizationCodeGrant(code)
+    .then(data => {
+            console.log('this is from the data')
+            res.json({
+                accessToken: data.body.access_token,
+                refreshToken: data.body.refresh_token,
+                expiresIn: data.body.expires_in,
+        })
+        })
+        .catch(err => {
+            console.log('this is from the error')
+            //res.sendStatus(400)
+        })
+
+    })
+
+
+
+
+app.listen(3001, () => {
+    console.log(`express app listening on port ${port}`)
 })
