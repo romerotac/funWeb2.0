88608e05613cbdd97e70f54a148544e5f47c777d
romerotac <christian.romero02.cr@gmail.com>
2022-07-24 12:27:53 -0400

    Added new CV.pdf

--- client/src/spotifyFiles/Login.js
+++ client/src/spotifyFiles/Login.js
@@ -1,6 +1,6 @@
 import React from "react";
 import './Login.css'
-const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=49fb612d070040f9a34236837ab79ad3&response_type=code&redirect_uri=http://localhost:3000&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
+const AUTH_URL = "https://accounts.spotify.com/authorize?client_id=49fb612d070040f9a34236837ab79ad3&response_type=code&redirect_uri=https://myfunweb.herokuapp.com&scope=streaming%20user-read-email%20user-read-private%20user-library-read%20user-library-modify%20user-read-playback-state%20user-modify-playback-state";
 
 export const Login = () => {
 
